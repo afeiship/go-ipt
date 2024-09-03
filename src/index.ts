4303104ecef2f0f2f4b3793f70fb800d1c7c70bf
afeiship <1290657123@qq.com>
2024-09-03 22:22:23 +0800

    feat: init partiail equal

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
-import fastDeepEqual from 'fast-deep-equal';
+import fde from 'fast-deep-equal';
+import filterKeysDeep from '@jswork/filter-keys-deep';
 
 export interface IsDataEqualOptions {
   ignoreKeys?: string[];
 }
 
+function filterByCommonKeys(obj1, obj2) {
+  const commonKeys = Object.keys(obj1).filter((key) =>
+    obj2.hasOwnProperty(key),
+  );
+  const filterKeys = (obj, keys) => {
+    if (Array.isArray(obj)) {
+      return obj.map((item) => filterKeys(item, keys));
+    } else if (obj !== null && typeof obj === 'object') {
+      return keys.reduce((acc, key) => {
+        if (obj.hasOwnProperty(key)) {
+          acc[key] = filterKeys(obj[key], keys);
+        }
+        return acc;
+      }, {});
+    }
+    return obj; // 基本类型或函数直接返回
+  };
+
+  return [filterKeys(obj1, commonKeys), filterKeys(obj2, commonKeys)];
+}
+
 const isDataEqual = (oldData: any, newData: any, options?: IsDataEqualOptions): boolean => {
   const { ignoreKeys = [] } = options || {};
-  let isEqual = true;
-  for (const key in newData) {
-    const oldValue = oldData[key];
-    const newValue = newData[key];
-    const hasUndefined = oldValue === undefined || newValue === undefined;
-    if (hasUndefined) continue;
-    if (ignoreKeys?.includes(key)) continue;
-    isEqual = fastDeepEqual(oldValue, newValue);
-    if (!isEqual) break;
-  }
-  return true;
+  const filteredObj1 = filterKeysDeep(oldData, ignoreKeys);
+  const filteredObj2 = filterKeysDeep(newData, ignoreKeys);
+
+  const [commonObj1, commonObj2] = filterByCommonKeys(
+    filteredObj1,
+    filteredObj2,
+  );
+  return fde(commonObj1, commonObj2);
 };
 
 export default isDataEqual;
